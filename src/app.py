533874be890e83a8d6808c39a42c779300b5fd93
Ursula Jardon <155091638+gar9312@users.noreply.github.com>
2024-09-03 02:28:38 +0000

    Finish Work

--- src/app.py
+++ src/app.py
@@ -12,6 +12,8 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
+
+
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
@@ -25,18 +27,49 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+# GET all members
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
+    return jsonify(members), 200
+
+# POST add new member
+@app.route('/member', methods=['POST'])
+def add_member():
+    body = request.get_json() 
+    new_member =  {
+        "id": body.get("id", jackson_family._generateId()),
+        "first_name": body.get("first_name", None),
+        "last_name": body.get("last_name", None),
+        "age": body.get("age", None),
+        "lucky_numbers": body.get("lucky_numbers", None)
     }
-
-
-    return jsonify(response_body), 200
+    if new_member["last_name"] != None:
+        return jsonify({"error": "The last name is generated automatically. Do not send in the body."}), 400
+    
+    for member in jackson_family._members:
+        if new_member["id"] == member["id"]:
+            return jsonify({"error": "The id is already in use"}), 400
+                                                                         
+    accepted_member = jackson_family.add_member(new_member)
+    return jsonify(accepted_member), 200
+    
+# GET one member
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+        member = jackson_family.get_member(member_id)
+        if member is None:
+            return jsonify({"error": "Member not found"}), 404
+        return jsonify(member), 200
+    
+# DELETE one member
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+        member = jackson_family.delete_member(member_id)
+        if member is True:
+            return jsonify({"done": True}), 200
+        return jsonify({"error": "Member not found"}), 404
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/utils.py
+++ src/utils.py
@@ -35,3 +35,4 @@ def generate_sitemap(app):
         <img src='https://github.com/breatheco-de/exercise-family-static-api/blob/master/rigo-baby.jpeg?raw=true' />
         <h1>Hello Rigo!!</h1>
         This is your api home, remember to specify a real endpoint path like: <ul style="text-align: left;">"""+links_html+"</ul></div>"
+
